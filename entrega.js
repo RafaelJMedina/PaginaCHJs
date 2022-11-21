348a4a9bcb8188baa5db36d4c21d0da07d569117
Rafael <rafaelmedina350@hotmail.com>
2022-11-21 18:04:49 -0300

    Agregando Busqueda y filtrado

--- entrega.js
+++ entrega.js
@@ -11,12 +11,12 @@ class Producto {
   }
 }
 
-const bicicleta = new Producto(0, "Bicicleta", 43999, 15);
-const tv = new Producto(1, "TV", 60000, 10);
-const reloj = new Producto(2, "Reloj", 5000, 30);
-const pdv = new Producto(3, "Placa de video", 150000, 20);
-const kitmate = new Producto(4, "Kit matero", 9500, 100);
-const pelota = new Producto(5, "Pelota del mundial", 10000, 500);
+const bicicleta = new Producto(0, "Bicicleta", 3000, 15);
+const tv = new Producto(1, "TV", 10000, 10);
+const reloj = new Producto(2, "Reloj", 1500, 30);
+const pdv = new Producto(3, "Placa de video", 15000, 20);
+const kitmate = new Producto(4, "Kit matero", 1000, 100);
+const pelota = new Producto(5, "Pelota del mundial", 800, 500);
 
 const productos = [bicicleta, tv, reloj, pdv, kitmate, pelota];
 
@@ -76,22 +76,56 @@ function agregarCarrito() {
     respuesta = parseInt(prompt(productoOfrecidos));
   }
   alert("Cerramos tu pedido");
-  mostrarCarrito()
+  mostrarCarrito();
+  ofertaCarrito();
 }
 
-
-let productosCarrito = `Vas a llevar: `
-let precioCarrito = 0
-
+let productosCarrito = `Vas a llevar: `;
+let precioCarrito = 0;
 
 agregarCarrito();
 
-
-function mostrarCarrito(){
-  for (itemsElegidos of carrito){
-    productosCarrito += ` \n - ${itemsElegidos.nombre}`
-    precioCarrito += itemsElegidos.precio
+function mostrarCarrito() {
+  for (itemsElegidos of carrito) {
+    productosCarrito += ` \n - ${itemsElegidos.nombre}`;
+    precioCarrito += itemsElegidos.precio;
   }
 
-  alert(`Tu Carrito: \n ${productosCarrito} \n Por un total de ${precioCarrito}`)
+  alert(
+    `Tu Carrito: \n ${productosCarrito} \n Por un total de ${precioCarrito}`
+  );
 }
+
+function ofertaCarrito() {
+  let ofertCarrito = `Tu carrito: `;
+  let ofertafinalCarrito = 0;
+  let descuento = 0;
+
+  seleccion = prompt("Hay productos en oferta!!! Deseas verlo? si - no");
+
+  for (itemsElegidos of carrito) {
+    ofertCarrito += ` \n - ${itemsElegidos.nombre}`;
+    ofertafinalCarrito += itemsElegidos.precio;
+  }
+
+  descuento = (ofertafinalCarrito / 100) * 20;
+
+  const ofertas = productos.filter((Producto) => {
+    if (Producto.precio > 9000 && seleccion === "si") {
+      alert(
+        `Ofertas: ${Producto.nombre} \n ${Producto.precio} \n Llevando 2 unidades la segunda tiene descuento del 50%!`
+      );
+    }
+  });
+
+  while (seleccion === "no") {
+    alert("Gracias por la compra!");
+    carrito.map((carritoFinal) => {
+      alert(
+        `Tus productos: \n ${ofertCarrito} \n Por un total de ${ofertafinalCarrito} con un 20% de descuento! de ${descuento}$`
+      );
+    });
+  }
+}
+
+//  else {return alert("No hay ofertas si no llevas mas de dos productos!");}
